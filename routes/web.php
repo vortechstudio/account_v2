91ce568645f859c8e569bfb1b3fc9b8da75c1a81
Vortechstudio <contact@vortechstudio.fr>
2024-05-02 01:17:12 +0200

    feat(auth): Implementation du login avec Google
    - Ajout de nouvelles routes pour la gestion de l'authentification
    - Ajout d'un nouveau contrôleur pour gérer l'authentification
    - Mise à jour du fichier composer.json pour ajouter la dépendance "laravel/socialite"
    - Mise à jour du fichier composer.lock suite à l'ajout de la dépendance "laravel/socialite"

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('auth')->as('auth.')->group(function () {
+    Route::get('login', [\App\Http\Controllers\Auth\AuthController::class, 'login'])->name('login');
+    Route::get('{provider}/redirect', [\App\Http\Controllers\Auth\AuthController::class, 'redirect'])->name('redirect');
+    Route::get('{provider}/callback', [\App\Http\Controllers\Auth\AuthController::class, 'callback'])->name('callback');
+    Route::get('{provider}/setup/{email}', \App\Livewire\Auth\SetupRegister::class)->name('setup-register');
+    Route::get('logout', [\App\Http\Controllers\Auth\AuthController::class, 'logout'])->name('logout');
+    Route::post('password-confirm', [\App\Http\Controllers\Auth\AuthController::class, 'confirmPassword'])
+        ->name('confirm-password')
+        ->middleware(['auth', 'throttle:6,1']);
+});
+
+Route::get('password-confirm', [\App\Http\Controllers\Auth\AuthController::class, 'confirmPasswordForm'])
+    ->name('password.confirm')
+    ->middleware('auth');
